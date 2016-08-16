bb7ac4c77c18b94e751d9e6407e5c09c167b06f6
Marta Sanchez and Thomas Rivas <marta@>
2016-08-16 16:30:47 -0700

    create the starting javascript code to return prime numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
 
+  $("form").submit(function(element){
+
+    event.preventDefault();
+
+    var number = parseInt($("input#number").val());
+
+    var primes = [];
+
+    for (var i = 2; i < number; i++) {
+      primes.push(i);
+    }
+
+    alert(primes);
+  })
+
+
+
+
+
+
+
 });
